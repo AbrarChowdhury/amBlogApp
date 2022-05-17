6c56f80e1572169138513514c1736fdda230c693
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-05-17 21:01:48 +0600

    make the cards display only 200 characters

--- client/src/components/blogCard/BlogCard.jsx
+++ client/src/components/blogCard/BlogCard.jsx
@@ -5,13 +5,15 @@ function BlogCard({title, content, date, _id}) {
   let navigate = useNavigate();
   return (
     <Card onClick={()=>navigate(`/blog/${_id}`)}>
-      <CardActionArea>
+      <CardActionArea sx={{minHeight:"200px"}}>
         <CardContent>
           <Typography variant="h2" component="div">
             {title}
           </Typography>
           <Typography variant="body2" color="text.secondary" gutterBottom>
-            {content}
+        
+            {content.substring(0, 200)} {content.length >= 200 && '...'}
+
           </Typography>
           <Typography variant="body1" gutterBottom>
             {date}
